d3dd43141bccf9f429b68d3b81a6155662dc90c6
luiz.bezerra <luizantoniopcb@gmail.com>
2024-09-16 20:59:00 -0300

    create folder backend

---
+++ projeto-intermediario/backend/src/main/java/com/backend/routerairplane/RouterairplaneApplication.java
@@ -0,0 +1,13 @@
+package com.backend.routerairplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterairplaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouterairplaneApplication.class, args);
+	}
+
+}
---
+++ projeto-intermediario/backend/src/test/java/com/backend/routerairplane/RouterairplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.routerairplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterairplaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
